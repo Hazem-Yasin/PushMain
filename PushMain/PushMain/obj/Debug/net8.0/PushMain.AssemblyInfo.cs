a384a2237e8ce755c6547c19cb4fd433f3b5bbd2
Hazem Yasin <hazem.yasin.1416@gmail.com>
2025-01-30 15:19:03 +0200

    added basic questions and answers on an html file

--- PushMain/PushMain/obj/Debug/net8.0/PushMain.AssemblyInfo.cs
+++ PushMain/PushMain/obj/Debug/net8.0/PushMain.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PushMain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+936fc2f63248bb42d57acefe6766d9e6a31c1f90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad2286228309f002e71c03e540046d9ee730447")]
 [assembly: System.Reflection.AssemblyProductAttribute("PushMain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PushMain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
